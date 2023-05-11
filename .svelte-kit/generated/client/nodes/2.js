644fac0e99337e4543c034502573784fef81dc87
Tyler J. Ferrara <tylerferrara123@gmail.com>
2023-05-10 22:00:22 -0600

    Remove stock svelte init helper

---
+++ .svelte-kit/generated/client/nodes/2.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../src/routes/stock/+layout.svelte";
---
+++ .svelte-kit/generated/client/nodes/5.js
@@ -0,0 +1,3 @@
+import * as universal from "../../../../src/routes/stock/+page.js";
+export { universal };
+export { default as component } from "../../../../src/routes/stock/+page.svelte";
