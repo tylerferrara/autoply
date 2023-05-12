98f527ea3d933d88e417dc6356f98dbd3daadaca
Tyler J. Ferrara <tylerferrara123@gmail.com>
2023-05-11 19:24:54 -0600

    create basic landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,30 @@
-<h1>Send your resume to ALL employers that meet your criteria.</h1>
+<script>
+	import './styles.css';
+</script>
 
-<h3>1. Create your application</h3>
-<h3>2. Choose job criteria</h3>
-<h3>3. Auto apply!</h3>
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Home page" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700;900&display=swap" rel="stylesheet">
 
-<a href="/begin">
-    <button>Let's go</button>
-</a>
+</svelte:head>
 
+<section class="center fill">
 
+    <div class="welcome-header">
+        <h1>Be the first to apply to jobs</h1>
+        <h1 class="center-text">with Autoply</h1>
+    </div>
+    
+    
+    <h3>1. Create your application</h3>
+    <h3>2. Choose job criteria</h3>
+    <h3>3. Auto apply!</h3>
+    
+    <a style="margin-top: 1rem;" href="/begin">
+        <button class="action-btn">Let's go</button>
+    </a>
+
+</section>
